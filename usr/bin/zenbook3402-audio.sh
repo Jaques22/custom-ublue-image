7c4c118b41a963f63ae0f7475c982cd2aadaf57b
Jaques22 <Jaques22@users.noreply.github.com>
2023-05-14 01:43:45 +0100

    put audio script files in tmp

--- usr/bin/zenbook3402-audio.sh
+++ usr/bin/zenbook3402-audio.sh
@@ -2,9 +2,9 @@
 # This script downloads and applies the required modifications to the ACPI tables in order for the sound to work on the ASUS Zenbook 3402za
 # and similar laptops using the same chip. 
 
-set -oue pipefail
+# set -oue pipefail
 
-mkdir ./audio-build && cd ./audio-build
+mkdir /tmp/audio-build && cd /tmp/audio-build
 curl https://raw.githubusercontent.com/thor2002ro/asus_zenbook_ux3402za/main/Sound/ssdt-csc3551.dsl -o ssdt-csc3551.dsl
 curl https://raw.githubusercontent.com/thor2002ro/asus_zenbook_ux3402za/main/Sound/01_acpi -o 01_acpi
 
